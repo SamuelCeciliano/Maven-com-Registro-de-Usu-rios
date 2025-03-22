a61787ce6384e4d42cea79bd5708689309202fad
Samuel Ceciliano <125198622+SamuelCeciliano@users.noreply.github.com>
2025-03-22 10:11:43 -0300

    Delete JPAHibernate/relacional directory

--- JPAHibernate/relacional/src/main/java/com/entidade/relacional/repository/ProfissaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.entidade.relacional.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entidade.relacional.model.Profissao;
-
-public interface ProfissaoRepository extends JpaRepository<Profissao, Long>{
-
-}
